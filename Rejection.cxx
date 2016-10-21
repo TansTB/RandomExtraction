17f5b848632c7a2e95bd0bad110f6662ce91236d
cthorbur <connor.thorburn@edu.unito.it>
2016-10-21 12:40:44 +0200

    Add files via upload

---
+++ Rejection.cxx
@@ -0,0 +1,41 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <Riostream.h>
+#include <TMath.h>
+#include <TH1I.h>
+#endif
+using namespace TMath;
+Double_t func(Double_t ,Double_t );
+void Rejection(Double_t a,Int_t n, Int_t nbins,Int_t seed){
+  gRandom->Delete();
+  gRandom = new TRandom3(seed);
+  Double_t beginning = 0;
+  Double_t end = 2*Pi();
+  TH1I* hist = new TH1I("hist","Rejection Method",nbins,beginning,end);
+  TStopwatch *timer = new TStopwatch();
+  Double_t m = 2;
+  Double_t u1, u2, xt, yt;
+  Int_t counter = 0;
+  while(counter < n){
+    u1 = gRandom -> Rndm();
+    u2 = gRandom -> Rndm();
+    xt = beginning + (end - beginning)*u1;
+    yt = m*u2;
+    if (func(a,xt) > yt){
+      hist->Fill(xt);
+      counter++;
+    }
+  }
+  timer->Stop();
+  using namespace std;
+  cout << "--------------------------------" << endl << "Rejection Method Statistics:" << endl;
+  timer->Print("u");
+  cout << "--------------------------------" << endl;
+  
+  return hist;
+}
+
+Double_t func(Double_t a, Double_t xx){
+Double_t f = 1/(sin(xx)*sin(xx)+a*cos(xx)*cos(xx));
+return f;
+} 
+
